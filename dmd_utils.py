9063acc206cb593d845af5ff5a5cb50057abd2dd
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-03-31 15:34:58 -0400

    add step size to getVideoFrames function

--- dmd_utils.py
+++ dmd_utils.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 from pydmd import DMD
 
-def getVideoFrames(filename, frames):
-    framen = frames[1] - frames[0]
+def getVideoFrames(filename, frames,step=1):
+    framen = int((frames[1] - frames[0])/step)+1
     video = cv2.VideoCapture(filename)
     #cv2.cvtColor(video, cv2.COLOR_BGR2RGB)
     frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -12,7 +12,7 @@ def getVideoFrames(filename, frames):
     ret = True
     fc = 0
     while fc<frames[1] and ret:
-        if fc < frames[0]:
+        if fc < frames[0] or (fc-frames[0])%step != 0:
             ret, image = video.read()
             fc+=1
             continue
@@ -23,7 +23,7 @@ def getVideoFrames(filename, frames):
             #original_image.show(title='original image')
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             vectorized = np.reshape(image, (frame_height*frame_width*3,))
-            matrix[fc-frames[0]] = vectorized
+            matrix[int((fc-frames[0])/step)] = vectorized
             fc+=1
     return matrix
 
